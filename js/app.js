f127b248f8e262e623e74d0896626ad5c9888f10
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-08-12 18:57:58 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+        let emailModal = document.getElementsByClassName('email-modal')[0];
+        let closeModal = document.getElementsByClassName('email-modal__button')[0];
+
+
+        closeModal.addEventListener("click", () => {
+            emailModal.classList.remove('email-modal--visible')
+        });
+
+        document.addEventListener('mouseleave', () => {
+            emailModal.classList.add('email-modal--visible')
+        });
+        console.log(document);
+
+      
+        }
