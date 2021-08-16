e6949123cb9cd5ad63fa3a849ad171aebe137f2f
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-08-15 19:05:53 -0500

    Update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 window.onload = function () {
-        let emailModal = document.getElementsByClassName('email-modal')[0];
-        let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+    let emailModal = document.getElementsByClassName('email-modal')[0];
+    let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+    let emailState = false;
+    let emailInput = document.getElementsByClassName('email-modal__input')[0];
+    let emailButton = document.getElementsByClassName('email-modal__button')[0];
 
-
-        closeModal.addEventListener("click", () => {
-            emailModal.classList.remove('email-modal--visible')
-        });
-
-        document.addEventListener('mouseleave', () => {
+    
+    
+    document.body.addEventListener('mouseleave', () => {
+        if (emailState == false) {
             emailModal.classList.add('email-modal--visible')
-        });
-        console.log(document);
-
-      
+            emailState = true
         }
+    })
+
+    closeModal.addEventListener("click", () => {
+        emailModal.classList.remove('email-modal--visible')
+
+    });
+
+    function emailIsValid (email){
+        return /\S+@\S+\.\S+/.test(email);
+    }
+    
+    emailButton.addEventListener('click', () => {
+        if(emailIsValid(emailInput.value)){
+            console.log(emailInput.value)
+        } else {
+            alert("invalid email")
+        }
+        }
+
+    )
+
+    console.log(document);
+
+
+
+
+}
